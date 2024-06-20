1d506c35a11ed7a7bcd702e9e3d735a5bd7648a5
Alex Yakimenko <alexyakimenkojr@gmail.com>
2024-06-21 00:02:15 +0300

    change root to user1 (my case)

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,2 +1,2 @@
-rsync ./index.js ./package.json root@194.146.25.179:/root/prebid_wrapper -r
-ssh root@194.146.25.179 'cd /root/prebid_wrapper && npm install && pm2 restart prebid_wrapper'
+rsync ./index.js ./package.json user1@194.146.25.179:/home/user1/prebid_wrapper -r
+ssh user1@194.146.25.179 'cd /home/user1/prebid_wrapper && npm install && pm2 restart prebid_wrapper'
