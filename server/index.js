b952f7d01fe386178a2ddbd53bc7bf5c7f6b3c12
Alex Yakimenko <alexyakimenkojr@gmail.com>
2024-06-26 22:30:52 +0300

    connect to clickhouse, insert values, get values & render em on page

--- server/index.js
+++ server/index.js
@@ -1,51 +1,19 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
-import { debounce } from "wrapperLib/src/debounce.js";
+import analytics from './routes/anal_router.js';
+
+
 const app = express();
 // export express.Router()
 // import {router } from './anal_router';
 // app.use(router)
+app.use(cors());
+app.use(express.static('./public'))
+app.use('/analytics', analytics)
 
 
 app.use(express.static('dist'));
-app.get('/', (req, res) => {
-    res.send('ok')
-})
-
-app.use(cors());
-const MAX_CACHE_LEN = 100_000;
-const eventsCache = [];
-function writeEvents(){
-
-    const events = eventsCache.slice();
-    eventsCache.length = 0;
-}
-
-const writeEventsDebounced = debounce(writeEvents, 1000 * 60);
-app.post('/analytics',bodyParser.json(), (req, res) => {
-    const events = req.body;
-    //validate
-
-    eventsCache.push(events);
-        if(eventsCache.length > MAX_CACHE_LEN){
-            writeEvents();
-        }else{
-             writeEventsDebounced()
-        }
-
-    res.status(204).end('');
-})
-
-
-app.get('/analytics', (req, res) => {
-    const {startDate,endDate, type, groupBy} = req.query;
-    // graph to show events by type , in 24 hours , grouped by hour
-    // new events from client:   bid request, bid response, impression      {Bids:[]{bidderCode,unitCode, cpm}}
-    res.json(eventsCache)
-})
-
 
 app.listen(8080, () => {
-    console.log('Server is running on port 80');
+    console.log('Server is running on port 8080');
 })
